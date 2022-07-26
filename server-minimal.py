623f4acf8a8cc66ae1077ce57f5bf51b206bb8dd
Gareth <dszjgsu@163.com>
2022-07-26 15:44:16 +0800

    add server opcua

---
+++ server-minimal.py
@@ -0,0 +1,42 @@
+
+import sys
+sys.path.insert(0, "..")
+import time
+
+
+from opcua import ua, Server
+
+
+if __name__ == "__main__":
+
+    # setup our server
+    server = Server()
+    server.set_endpoint("opc.tcp://0.0.0.0:48400/freeopcua/server/")
+
+    # setup our own namespace, not really necessary but should as spec
+    uri = "http://examples.freeopcua.github.io"
+    idx = server.register_namespace(uri)
+
+    # get Objects node, this is where we should put our nodes
+    objects = server.get_objects_node()
+
+    # populating our address space
+    myobj = objects.add_object(idx, "MyObject")
+    myvar = myobj.add_variable(idx, "MyVariable", 6.7)
+    faultMsg = myobj.add_variable(idx, "FaultMsg", 1)
+    myvar.set_writable()    # Set MyVariable to be writable by clients
+    faultMsg.set_writable()
+
+    # starting!
+    server.start()
+    
+    try:
+        count = 0
+        while True:
+            time.sleep(1)
+            count += 0.1
+            myvar.set_value(count)
+            faultMsg.set_value("turbine error %s" % count*10)
+    finally:
+        #close connection, remove subcsriptions, etc
+        server.stop()
